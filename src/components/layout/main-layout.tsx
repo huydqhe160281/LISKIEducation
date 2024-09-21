18a64967f2384776ebae3fc1bcef1706c4398634
huydqhe160281 <huydqhe160281@fpt.edu.vn>
2024-09-21 23:28:27 +0700

    update: chat provider

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -2,6 +2,7 @@ import React, { FC, ReactNode } from 'react'
 import Box from '@mui/material/Box'
 import { Footer } from '@/components/footer'
 import { Header } from '@/components/header'
+import Script from 'next/script'
 
 interface Props {
   children: ReactNode
@@ -9,11 +10,36 @@ interface Props {
 
 const MainLayout: FC<Props> = ({ children }) => {
   return (
-    <Box component="main">
-      <Header />
-      {children}
-      <Footer />
-    </Box>
+    <>
+      <html lang="en">
+        <body>
+          {children}
+          {/* Nhúng mã Tawk.to */}
+          <Script
+            id="tawkto"
+            strategy="afterInteractive" // Giúp mã chỉ tải sau khi trang đã tải
+            dangerouslySetInnerHTML={{
+              __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/66eef278e5982d6c7bb26457/1i8am6avq';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+            }}
+          />
+        </body>
+      </html>
+      <Box component="main">
+        <Header />
+        {children}
+        <Footer />
+      </Box>
+    </>
   )
 }
 
