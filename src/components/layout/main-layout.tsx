9aa0b1cb42ce3794336fcc4726d31d437977403d
huydqhe160281 <huydqhe160281@fpt.edu.vn>
2024-09-21 23:32:16 +0700

    update: chat provider

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -10,36 +10,30 @@ interface Props {
 
 const MainLayout: FC<Props> = ({ children }) => {
   return (
-    <>
-      <html lang="en">
-        <body>
-          {children}
-          {/* Nhúng mã Tawk.to */}
-          <Script
-            id="tawkto"
-            strategy="afterInteractive" // Giúp mã chỉ tải sau khi trang đã tải
-            dangerouslySetInnerHTML={{
-              __html: `
-              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-              (function(){
+    <Box component="main">
+      <Header />
+      {children}
+      <Footer />
+
+      {/* Nhúng mã Tawk.to */}
+      <Script
+        id="tawkto"
+        strategy="afterInteractive" // Chỉ tải sau khi trang đã tải xong
+        dangerouslySetInnerHTML={{
+          __html: `
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
               var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
               s1.async=true;
               s1.src='https://embed.tawk.to/66eef278e5982d6c7bb26457/1i8am6avq';
               s1.charset='UTF-8';
               s1.setAttribute('crossorigin','*');
               s0.parentNode.insertBefore(s1,s0);
-              })();
-            `,
-            }}
-          />
-        </body>
-      </html>
-      <Box component="main">
-        <Header />
-        {children}
-        <Footer />
-      </Box>
-    </>
+            })();
+          `,
+        }}
+      />
+    </Box>
   )
 }
 
