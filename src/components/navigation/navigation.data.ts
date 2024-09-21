e5b0e293c239608202af1f14fb8405f3fcfff33e
huydqhe160281 <huydqhe160281@fpt.edu.vn>
2024-09-21 23:10:11 +0700

    first commit

---
+++ src/components/navigation/navigation.data.ts
@@ -0,0 +1,20 @@
+import type { Navigation } from '@/interfaces/navigation'
+
+export const navigations: Navigation[] = [
+  {
+    label: 'Trang chủ',
+    path: '#', // '/',
+  },
+  {
+    label: 'Khóa học',
+    path: 'popular-course', // '/popular-course',
+  },
+  {
+    label: 'Đánh giá',
+    path: 'testimonial', // '/testimonial',
+  },
+  {
+    label: 'Giảng viên',
+    path: 'mentors', // '/mentors',
+  },
+]
