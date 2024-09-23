6797888408407ae7bf270ace498d4f01eb594074
huydqhe160281 <huydqhe160281@fpt.edu.vn>
2024-09-23 22:39:41 +0700

    fix: favico and image

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        destination: 'https://liskieducation.vercel.app/:path*',
-        permanent: true,
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
